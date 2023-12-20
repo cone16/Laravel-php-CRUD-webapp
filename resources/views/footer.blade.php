e8f2b49af26b641d7e5f29f1c996b1f029f0fa0b
Cone16 <c.one12@web.de>
2023-12-20 21:10:35 +0100

    little texts in footer.

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-dark bg-dark">
     <a class="navbar-brand" href="#" style="font-size: 14px; padding-left: 20px;">
-        Copyright LuLeCon.de®
+        LuLeCon.info
     </a>
 </nav>
