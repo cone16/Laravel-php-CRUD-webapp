440d03ee60b59bfec71451c1f80361e01b7da470
Cone16 <c.one12@web.de>
2023-12-25 21:17:02 +0100

    removed jenssegers Agent

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -30,7 +30,6 @@ return array(
     'App\\Http\\Middleware\\TrustProxies' => $baseDir . '/app/Http/Middleware/TrustProxies.php',
     'App\\Http\\Middleware\\ValidateSignature' => $baseDir . '/app/Http/Middleware/ValidateSignature.php',
     'App\\Http\\Middleware\\VerifyCsrfToken' => $baseDir . '/app/Http/Middleware/VerifyCsrfToken.php',
-    'App\\Models\\F1_larav' => $baseDir . '/app/Models/F1_larav.php',
     'App\\Models\\User' => $baseDir . '/app/Models/User.php',
     'App\\Providers\\AppServiceProvider' => $baseDir . '/app/Providers/AppServiceProvider.php',
     'App\\Providers\\AuthServiceProvider' => $baseDir . '/app/Providers/AuthServiceProvider.php',
